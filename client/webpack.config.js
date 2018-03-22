09684f1d55b7da7c05c2dba67bcd089daccfedcc
Nicholas March <n.march213@gmail.com>
2018-03-21 21:34:39 -0500

    Allowed for statically serving the Vue js app with express

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,10 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, '../dist'),
     publicPath: '/dist/',
     filename: 'build.js'
   },
@@ -12,26 +12,15 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
+        use: ['vue-style-loader', 'css-loader']
       },
       {
         test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader'
-        ],
+        use: ['vue-style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.sass$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader?indentedSyntax'
-        ],
+        use: ['vue-style-loader', 'css-loader', 'sass-loader?indentedSyntax']
       },
       {
         test: /\.vue$/,
@@ -41,16 +30,8 @@ module.exports = {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': [
-              'vue-style-loader',
-              'css-loader',
-              'sass-loader'
-            ],
-            'sass': [
-              'vue-style-loader',
-              'css-loader',
-              'sass-loader?indentedSyntax'
-            ]
+            scss: ['vue-style-loader', 'css-loader', 'sass-loader'],
+            sass: ['vue-style-loader', 'css-loader', 'sass-loader?indentedSyntax']
           }
           // other vue-loader options go here
         }
@@ -71,7 +52,7 @@ module.exports = {
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      vue$: 'vue/dist/vue.esm.js'
     },
     extensions: ['*', '.js', '.vue', '.json']
   },
@@ -84,10 +65,10 @@ module.exports = {
     hints: false
   },
   devtool: '#eval-source-map'
-}
+};
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  module.exports.devtool = '#source-map';
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
@@ -104,5 +85,5 @@ if (process.env.NODE_ENV === 'production') {
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
-  ])
+  ]);
 }
--- server/index.js
+++ server/index.js
@@ -1,31 +1,12 @@
 var express = require('express');
 var path = require('path');
+var serveStatic = require('serve-static');
 var app = express();
 
 var port = 3000;
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get("/", function(req, res) {
-
-	res.render("index");
-
-});
-
-app.get("/about", function(req, res) {
-
-	res.render("about");
-
-});
-
-app.get("/blog", function(req, res) {
-
-	res.render("blog");
-
-});
+app.use('/', serveStatic(path.join(__dirname, '../dist')));
 
 app.listen(port, function() {
-	console.log('Listening on port:', port);
+  console.log('Listening on port:', port);
 });
